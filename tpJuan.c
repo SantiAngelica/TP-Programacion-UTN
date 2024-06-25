d365a94ed20018565f701b246cf59b1f59b83ba2
Juan Sosa <juanmasosa162@gmail.com>
2024-06-25 17:58:16 -0300

    Subida de tpJuan.c

--- tpJuan.c
+++ tpJuan.c
@@ -16,12 +16,18 @@ int RECARGO_TARJETA = 0.05;
 
 int CAPACIDAD_MAXIMA = 60;
 
+char dni[CAPACIDAD_MAXIMA][9];
+char apellido[CAPACIDAD_MAXIMA][50];
+char nombre[CAPACIDAD_MAXIMA][50];
+int edad[CAPACIDAD_MAXIMA];
+char codigo_destino[CAPACIDAD_MAXIMA][4];
+int cuposBRA = 0, cuposMDQ = 0, cuposMZA = 0, cuposBRC = 0;
+
+int total_pasajeros;
 
 int cant_pasajeros;
 
-
-
-
+void ordenar_por_destino(char (*pasajeros)[5][50]);
 
 int main() {
     int opcion;
@@ -67,4 +73,133 @@ int main() {
     } while (opcion != 6);
     
     return 0;
+}
+
+void cargar_pasajeros(char (*pasajeros)[5][50]){
+    
+    for (int i = 0; i < cant_pasajeros; i++)
+    {
+        do
+        {
+            printf("Ingrese su DNI: ");
+            scanf("%s", dni[i]);
+            if (!validar_dni(dni[i]))
+            {
+                printf("El DNI es invalido, ingrese nuevamente. Recuerda, debe contener entre 7 y 8 dígitos y debe comenzar con 5, 6, y desde 10 hasta 60");
+            }
+            
+        } while (!validar_dni(dni[i]));
+        
+        printf("Ingrese su apellido: ");
+        scanf("%s",apellido[i]);
+
+        printf("Ingrese su nombre: ");
+        scanf("%s",nombre[i]);
+
+        printf("Ingrese su edad: ");
+        scanf("%i",edad[i]);
+
+        printf("Ingrese Código de Destino (BRA, MDQ, MZA, BRC): ");
+        scanf("%s", codigo_destino[i]);
+
+        while (strcmp(codigo_destino[i], "BRA") != 0 && strcmp(codigo_destino[i], "MDQ") != 0 && strcmp(codigo_destino[i], "MZA") != 0 && strcmp(codigo_destino[i], "BRC") != 0) {
+            printf("Codigo erroneo, ingrese de nuevo(BRA, MDQ, MZA, BRC): ");
+            scanf("%s", codigo_destino[i]);
+        }
+
+        if ( strcmp(codigo_destino[i], "BRA") == 0 )
+        {
+            if (cuposBRA > 60)
+            {
+                printf("Capacidad maxima alcanzada para Brasil!");
+            } 
+            else {
+                cuposBRA ++;
+            }
+        } else if (( strcmp(codigo_destino[i], "MDQ") == 0 ))
+        {
+            if (cuposMDQ > 60)
+            {
+                printf("Capacidad maxima alcanzada para Mar del Plata!");
+            } 
+            else {
+                cuposMDQ ++;
+            }
+        } else if (( strcmp(codigo_destino[i], "MZA") == 0 ))
+        {
+            if (cuposMZA > 60)
+            {
+                printf("Capacidad maxima alcanzada para Mendoza!");
+            } 
+            else {
+                cuposMZA ++;
+            }
+        } else if (( strcmp(codigo_destino[i], "BRC") == 0 ))
+        {
+            if (cuposBRC > 60)
+            {
+                printf("Capacidad maxima alcanzada para Bariloche!");
+            } 
+            else {
+                cuposBRC ++;
+            }
+        }
+        
+        total_pasajeros++;
+    }
+    
+}
+
+void ordenar_por_destino(char (*pasajeros)[5][50]){
+    int pos_menor;
+    
+    for (int i = 0; i < total_pasajeros - 1 ; i++)
+    {
+        pos_menor = i;
+
+        for (int j = i + 1; i < total_pasajeros; i++)
+        {
+            if (strcmp(codigo_destino[i],codigo_destino[j] > 0 || strcmp(codigo_destino[i],codigo_destino[j] == 0 && strcmp(apellido[i], apellido[j]) > 0)))
+            {
+                // Intercambiar codigo_destino
+                char aux[50];
+                strcpy(aux, codigo_destino[i]); 
+                strcpy(codigo_destino[i], codigo_destino[j]); 
+                strcpy(codigo_destino[j], aux);
+
+                // Intercambiar apellido
+                strcpy(aux, apellido[i]); 
+                strcpy(apellido[i], apellido[j]); 
+                strcpy(apellido[j], aux); 
+
+                // Intercambiar nombre
+                strcpy(aux, nombre[i]); 
+                strcpy(nombre[i], nombre[j]); 
+                strcpy(nombre[j], aux);
+
+                // Intercambiar dni
+                strcpy(aux, dni[i]); 
+                strcpy(dni[i], dni[j]); 
+                strcpy(dni[j], aux); 
+
+                // Intercambiar edad
+                int auxEdad;
+                auxEdad = edad[i]; 
+                edad[i] = edad[j];
+                edad[j] = auxEdad; 
+
+            }
+            
+        }
+        
+    }
+
+    
+}
+    
+
+void mostrar_lista_ordenada_por_destino(char (*pasajeros)[5][50]) {
+    for (int i = 0; i < total_pasajeros; i++) {
+        printf("%s - %s %s - DNI: %s - Edad: %d\n", codigo_destino[i], apellido[i], nombre[i], dni[i], edad[i]);
+    }
 }
